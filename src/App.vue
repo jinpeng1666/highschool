59a354bb02690f46e29918a3bfb77e24bfc54f55
JP.Zhou <3214423826@qq.com>
2024-02-11 15:39:11 +0800

    就随便添加了点主题颜色

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <el-container>
     <el-header>导航</el-header>
     <el-main>内容（路由）</el-main>
-    <el-footer>注脚</el-footer>
+    <el-footer height="100px">注脚</el-footer>
   </el-container>
 </template>
 
@@ -15,21 +15,14 @@ export default {
 <style scoped>
 .el-header,
 .el-footer {
-  background-color: #b3c0d1;
+  background-color: #67c23a;
   color: #333;
   text-align: center;
   line-height: 60px;
 }
 
-.el-aside {
-  background-color: #d3dce6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
 .el-main {
-  background-color: #e9eef3;
+  background-color: #e1f3d8;
   color: #333;
   text-align: center;
   line-height: 160px;
@@ -37,14 +30,5 @@ export default {
 
 body > .el-container {
   margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
 }
 </style>
